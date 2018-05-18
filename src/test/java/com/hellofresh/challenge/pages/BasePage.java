bb5cff2ed072e82e4bfab132898ddd72b6149c18
Camilo Nieto <camilo.nieto@auto1.com>
2018-05-18 15:52:53 +0200

    added a simple configuration reader for selecting the browser used for the test and added the geckodriver

--- src/test/java/com/hellofresh/challenge/pages/BasePage.java
+++ src/test/java/com/hellofresh/challenge/pages/BasePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage {
+public abstract class BasePage {
 
     WebDriver driver;
     WebDriverWait wait;
--- src/test/java/com/hellofresh/challenge/tests/BaseTest.java
+++ src/test/java/com/hellofresh/challenge/tests/BaseTest.java
@@ -1,11 +1,19 @@
 package com.hellofresh.challenge.tests;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+
+@Slf4j
 public abstract class BaseTest {
 
     WebDriver driver;
@@ -16,12 +24,40 @@ public abstract class BaseTest {
     String existingUserFullName = "Joe Black";
 
     @Before
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        driver = new ChromeDriver();
+    public void setUp() throws ConfigurationException {
+
+        String browser;
+        Configurations configs = new Configurations();
+
+        try {
+
+            PropertiesConfiguration config = configs.properties(new File("test.properties"));
+            browser = config.getString("driver.browser");
+            log.info("driver.browser: {}", browser);
+
+        } catch (ConfigurationException e) {
+
+            log.error("Something went wrong while reading the configuration file :(");
+            throw e;
+
+        }
+
+        driver = getDriver(browser);
 
         wait = new WebDriverWait(driver, 10, 50);
         driver.get("http://automationpractice.com/index.php");
+    }
+
+    private WebDriver getDriver(String browser) {
+        if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver");
+            return new FirefoxDriver();
+        } else if(browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+            return new ChromeDriver();
+        } else {
+            throw new RuntimeException(String.format("Browser %s is not supported", browser));
+        }
     }
 
     @After
